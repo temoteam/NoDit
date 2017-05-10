3858e1d985ef4e8ff660bda7314498423c1ac414
alexsvdk <alex.svdk@gmail.com>
2017-05-11 00:27:06 +0300

    Days sort

--- app/src/main/java/ru/temoteam/nodit/View/DaysActivity.java
+++ app/src/main/java/ru/temoteam/nodit/View/DaysActivity.java
@@ -1,53 +1,30 @@
 package ru.temoteam.nodit.View;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-
-import de.codecrafters.tableview.TableView;
-import de.codecrafters.tableview.model.TableColumnWeightModel;
-import ru.temoteam.nodit.Adapters.MTableDataAdapter;
 import ru.temoteam.nodit.Adapters.MyArrayAdapter;
-import ru.temoteam.nodit.Code.Day;
 import ru.temoteam.nodit.Code.Global;
 import ru.temoteam.nodit.R;
 
 public class DaysActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
+    private DayFragment dayFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,26 +33,12 @@ public class DaysActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
         tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
-
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
 
     }
 
@@ -102,40 +65,26 @@ public class DaysActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
+
+    public static class DayFragment extends Fragment {
+
         private static final String ARG_SECTION_NUMBER = "section_number";
+        private int sector;
 
-        public PlaceholderFragment() {
+
+
+        public DayFragment init (int sector){
+            this.sector=sector;
+            return this;
         }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            Log.d("sectionNumber", "" +sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
+
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_days, container, false);
             ListView listView = (ListView) rootView.findViewById(R.id.daysList);
-
-            MyArrayAdapter arrayAdapter = new MyArrayAdapter(getContext(),Global.days.get(2).lessons);
-
+            MyArrayAdapter arrayAdapter = new MyArrayAdapter(getActivity(),Global.days.get(sector).lessons);
             listView.setAdapter(arrayAdapter);
             return rootView;
         }
@@ -150,9 +99,7 @@ public class DaysActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            return new DayFragment().init(position);
         }
 
         @Override
